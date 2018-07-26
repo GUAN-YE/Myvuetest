025ce2f3cf04b046ff9ca562a113a6bbe1274ce0
GUAN-YE <15670549987@163.com>
2018-07-26 21:23:11 +0800

    Myvuetest

---
+++ Myvuetest/firstvue/dd.md
@@ -0,0 +1,3 @@
+1.v-model 指令
+全局安装
+npm install -g vue-cli
---
+++ Myvuetest/firstvue/src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'axios'
+Vue.config.productionTip = false
+
+// 全局接口中间件
+Vue.prototype.$http = Axios;
+Axios.interceptors.request.use(
+	config => {
+		let tbkt_token = localStorage.getItem("Tbkt-Token") || '';
+		if(tbkt_token) config.headers['Tbkt-Token'] = tbkt_token;
+		return config
+	},
+	err => {
+		return Promise.reject(err)
+	}
+);
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
